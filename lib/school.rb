a35d020a3c8c2bfbb620b51fd5b627d39dcd7ff4
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-19 20:33:13 -0800

    commit tree 21a2d497d6290ce357661ce14689ba0061ebee6f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+# require 'pry'
+
+class School # this is a model
+    
+    attr_accessor :roster
+    attr_reader :name
+
+    @@roster = {}
+
+    def initialize(name, grade)
+        @name = name
+        @grade = grade
+        @@roster[@grade]
+        @@roster[@grade] = []
+    end
+
+    def self.roster
+        @@roster
+    end
+
+end
